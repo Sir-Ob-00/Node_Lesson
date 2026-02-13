93a1204be663db0a44054962aaa5b999ab42b82f
Owusu_Boateng <owusuboat05@gmail.com>
2026-02-13 00:47:51 +0000

    initial commit

---
+++ intro.js
@@ -0,0 +1,8 @@
+//logs my name to the console
+console.log('My name is Boateng Owusu');
+
+//logs my age
+console.log('I am 25 years old');
+
+//logs my favorite programming language
+console.log('My favorite programming language is JavaScript');
