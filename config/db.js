ee02b55327188d80008e29d243443fa9cb393b40
Owusu_Boateng <owusuboat05@gmail.com>
2026-02-17 14:54:05 +0000

    user collection created

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async (url) => {
     try {
-        await mongoose.connect(url);
+        await mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
         console.log("Connected to MongoDB successfully");
     } catch (err) {
         console.error("Error connecting to MongoDB:", err);
